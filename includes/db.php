90deb2f82cefabc93e136506c33ef3344f9227af
jlrguy2017 <80444619+jlrguy2017@users.noreply.github.com>
2023-02-06 21:36:30 -0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,7 @@
 <?php
-$conn = pg_connect("host=127.0.0.1 port=25060 dbname=music user=music");
-if (!$conn) {
-    echo "Database connection nono working";
+$conn_string = "host=app-40e4fe2d-9e67-44f6-abf6-0b906486c9d7-do-user-13312887-0.b.db.ondigitalocean.com port=25060 dbname=defaultdb user=doadmin password=AVNS_V0C0GirTQV99QSXrLsa sslmode=require";
+$conn = pg_connect($conn_string);
+
+if(!$conn){
+    echo "Database connection faildiZZ";
 }
