29ced1d5360b846c128a480dff258e820de1c253
jlrguy2017 <80444619+jlrguy2017@users.noreply.github.com>
2023-02-06 21:27:02 -0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("host=localhost port=25060 dbname=music user=music");
+$conn = pg_connect("host=127.0.0.1 port=25060 dbname=music user=music");
 if (!$conn) {
     echo "Database connection nono working";
 }
