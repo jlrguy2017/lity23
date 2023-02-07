ab74f5ae10bd4809d975faeba57c58c17de4dbd6
jlrguy2017 <80444619+jlrguy2017@users.noreply.github.com>
2023-02-07 02:20:34 -0800

    Update register.php

--- register.php
+++ register.php
@@ -5,17 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Register | Music Sharing Platfrom</title>
+    <title>Register | LityTune</title>
 </head>
 <body>
     <center>
         <div class="logindiv">
-        <img class="logo" src="media/logo.png">
+        <img class="logo" src="logo01.gif" alt="LityTune Logo">
             <form class="loginform" action="includes/auth.php" method="post">
-                <input class="logininp" type="text" name="name" placeholder="Full Name"><br>
-                <input class="logininp" type="tel" name="phone" placeholder="Phone No."><br>
-                <input class="logininp" type="email" name="email" placeholder="Email Id" ><br>
-                <input class="logininp" type="password" name="passkey" placeholder="Password"><br>
+                <input class="logininp" type="text" name="name" placeholder="Full Name" required><br>
+                <input class="logininp" type="tel" name="phone" placeholder="Phone No." required><br>
+                <input class="logininp" type="email" name="email" placeholder="Email Id" required><br>
+                <input class="logininp" type="password" name="passkey" placeholder="Password" required><br>
                 <input class="loginbtn" type="submit" name="register" value="Register">
             </form>
             <p>Already Registered? <a href="index.php">Login Now</a></p>
