45ec4a866cdd8a84532c0594aa5e11a025f02c9b
jlrguy2017 <80444619+jlrguy2017@users.noreply.github.com>
2023-02-07 02:48:25 -0800

    Update addmusic.php

--- addmusic.php
+++ addmusic.php
@@ -6,25 +6,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Add Music | Music Sharing Platfrom</title>
+    <title>Add Music | LityTune</title>
 </head>
 <body>
+    <div class="logo-container">
+        <img class="logo" src="logo01.gif">
+    </div>
     <center>
         <div class="addmusicdiv">
         <?php user();?>
-            <a class="musica" href="editprofile.php">Edit Profile</a>
-            <a class="musica" href="addmusic.php">Add Music</a>
-            <a class="musica" href="deletemusic.php">Delete Music</a>
-            <a class="musica" href="logout.php">Logout</a><br>
+            <div class="navigation">
+                <a class="musica" href="editprofile.php">Edit Profile</a>
+                <a class="musica" href="addmusic.php">Add Music</a>
+                <a class="musica" href="deletemusic.php">Delete Music</a>
+                <a class="musica" href="logout.php">Logout</a>
+            </div>
             <form class="addmusicform" action="includes/music.php" method="post" enctype="multipart/form-data">
                 <input class="addmusicinp" type="text" name="title" placeholder="Music Title"><br>
                 <input class="addmusicinp" type="text" name="description" placeholder="Music Description"><br>
                 <select class="addmusicsel" name="category">
                     <option>Choose Category</option>
-                    <option value="Action">Action</option>
-                    <option value="Romantic">Romantic</option>
-                    <option value="Horror">Horror</option>
-                    <option value="Spanish">Kids</option><br>
+                    <option value="EDM">EDM</option>
+                    <option value="Hip Hop">Hip Hop</option>
+                    <option value="Religious">Religious</option>
+                    <option value="Metal">Metal</option><br>
                 </select><br>
                 <select class="addmusicsel" name="language">
                     <option>Choose Language</option>
